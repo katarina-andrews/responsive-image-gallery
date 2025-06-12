d42444169e099fae39365ac182cc0aba1c057b39
katarina-andrews <andrews.katarina@gmail.com>
2025-06-11 20:51:19 -0500

    added starter code, imported tailwindcss, added tailwind styling to elements

---
+++ script.js
@@ -0,0 +1,31 @@
+const gallery = document.getElementById("gallery");
+
+// Fetch 12 random images from Lorem Picsum
+
+async function fetchImages() {
+  try {
+    const res = await fetch("https://picsum.photos/v2/list?page=2&limit=50");
+
+    const data = await res.json();
+
+    gallery.innerHTML = data
+
+      .map((image) => {
+        return `
+
+ <div class="w-full h-60 rounded-lg shadow-2xl overflow-hidden hover:scale-105 transition duration-300 ease-in-out">
+
+ <img src="${image.download_url}" alt="Random image by ${image.author}"/>
+
+ </div>
+
+ `;
+      })
+
+      .join("");
+  } catch (error) {
+    console.error("Failed to fetch images:", error);
+  }
+}
+
+fetchImages();
